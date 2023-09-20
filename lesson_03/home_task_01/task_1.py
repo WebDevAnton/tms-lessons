c444babd24241068a420cefb2ffddbc961714fcd
Anton Kavaliou <r1ntsler66@gmail.com>
2023-09-20 22:03:39 +0200

    test lesson 3

--- lesson_03/home_task_01/task_1.py
+++ lesson_03/home_task_01/task_1.py
@@ -1,5 +1,6 @@
-n = int(input('Введите число: '))
-if n % 2 == 0:
-    print('True')
-else:
-    print('False')
+a = int(input('Введите значение стороны квадрата:'))
+p = 4 * a
+s = a ** 2
+d = a * (2 ** 0.5)
+tuple_a = (p, s, d)
+print(str(tuple_a[0]) + ', ' + str(tuple_a[1]) + ', ' + str(tuple_a[2]) + '.')
