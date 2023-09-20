ef5a2f00c56780e6d917af5118f5d4702e784b36
Anton Kavaliou <r1ntsler66@gmail.com>
2023-09-20 22:00:57 +0200

    test lesson 3

---
+++ lesson_03/home_task_01/task_0.py
@@ -0,0 +1,6 @@
+a = int(input('Введите значение стороны квадрата:'))
+p = 4 * a
+s = a ** 2
+d = a * (2 ** 0.5)
+tuple_a = (p, s, d)
+print(str(tuple_a[0]) + ', ' + str(tuple_a[1]) + ', ' + str(tuple_a[2]) + '.')
