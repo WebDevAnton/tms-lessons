f60ee4a095eb7da7eae3d9eb4b4977a3ea155fbf
Anton Kavaliou <r1ntsler66@gmail.com>
2023-09-27 04:48:35 +0200

    lesson 3

--- lesson_03/home_task_01/task_1.py
+++ lesson_03/home_task_01/task_1.py
@@ -1,6 +1,6 @@
 a = int(input('Введите значение стороны квадрата:'))
 per = 4 * a
-sqr = a ** 2
+sqr = a ** 3
 diag = a * (2 ** 0.5)
 tuple_a = (per, sqr, diag)
 print(str(tuple_a[0]) + ', ' + str(tuple_a[1]) + ', ' + str(tuple_a[2]) + '.')
