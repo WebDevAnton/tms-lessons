342e8888899a7936426003810fdc26358a0a8776
Anton Kavaliou <40646004+WebDevAnton@users.noreply.github.com>
2023-10-23 20:45:41 +0200

    Delete lesson_09 directory

--- lesson_09/homework/best_friend.py
+++
@@ -1,7 +0,0 @@
-from person import Person
-
-my_best_friend = Person('Ivan Ivanov', 20, 'M')
-my_best_friend.print_person_info()
-
-birth_year = my_best_friend.get_birth_year()
-print(f"Birth Year: {birth_year}")
